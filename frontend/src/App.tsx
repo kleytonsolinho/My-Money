f16787a5d33fcb7b3a724e1159859f8dcf1078da
Kleyton Solinho <kleytonsolinho@gmail.com>
2021-08-15 21:46:13 -0300

    modal styled

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,32 @@
+import Modal from 'react-modal';
+import { useState } from 'react';
 import { Header } from "./components/Header"
 import { Dashboard } from "./components/Dashboard"
 import { GlobalStyles } from "./styles/global"
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root');
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true);
+  }
+
+  function handleCloseNewTransactionModal() {
+    setIsNewTransactionModalOpen(false);
+  }
+
   return (
     <>
       <GlobalStyles />
-      <Header />
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
+      <NewTransactionModal
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
     </>
   );
 }
